710aa87ee3b7d673bf2c9f47bdf353d4bf93f322
XeinTDM <mjaow2020@gmail.com>
2023-08-09 01:07:15 +0200

    Reworked the GUI to more modern style, a dropdown with New/Open/Save + Cut/Copy/Paste, and also added the toolbox on the left side.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,16 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
 using System.Windows.Ink;
 using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace PureSketch
 {
     public partial class MainWindow : Window
     {
-        private readonly StrokeCollection _undoStrokes = new();
+        private readonly StrokeCollection _undoStrokes = new StrokeCollection();
+        private StrokeCollection _clipboardStrokes = new StrokeCollection();
 
         public MainWindow()
         {
@@ -51,8 +54,77 @@ namespace PureSketch
 
         private void OnThicknessChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            paintCanvas.DefaultDrawingAttributes.Width = thicknessSlider.Value;
-            paintCanvas.DefaultDrawingAttributes.Height = thicknessSlider.Value;
+            if (paintCanvas != null && thicknessSlider != null)
+            {
+                paintCanvas.DefaultDrawingAttributes.Width = thicknessSlider.Value;
+                paintCanvas.DefaultDrawingAttributes.Height = thicknessSlider.Value;
+            }
+        }
+
+        private void OnNewClick(object sender, RoutedEventArgs e)
+        {
+            paintCanvas.Strokes.Clear();
+        }
+
+        // Save Canvas Strokes to a File
+        private void OnSaveClick(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Ink Files (*.ink)|*.ink"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using var fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                paintCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        // Load Canvas Strokes from a File
+        private void OnOpenClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Ink Files (*.ink)|*.ink"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using var fs = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+                StrokeCollection strokes = new StrokeCollection(fs);
+                paintCanvas.Strokes = strokes;
+                fs.Close();
+            }
+        }
+
+        // Cut Selected Strokes
+        private void OnCutClick(object sender, RoutedEventArgs e)
+        {
+            if (paintCanvas.GetSelectedStrokes().Count == 0) return;
+
+            _clipboardStrokes = new StrokeCollection(paintCanvas.GetSelectedStrokes());
+            paintCanvas.Strokes.Remove(paintCanvas.GetSelectedStrokes());
+        }
+
+        // Copy Selected Strokes
+        private void OnCopyClick(object sender, RoutedEventArgs e)
+        {
+            if (paintCanvas.GetSelectedStrokes().Count == 0) return;
+
+            _clipboardStrokes = new StrokeCollection(paintCanvas.GetSelectedStrokes());
+        }
+
+        // Paste Strokes
+        private void OnPasteClick(object sender, RoutedEventArgs e)
+        {
+            if (_clipboardStrokes.Count == 0) return;
+
+            foreach (var stroke in _clipboardStrokes)
+            {
+                paintCanvas.Strokes.Add(stroke.Clone());
+            }
         }
     }
 }
